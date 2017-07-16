c14a94f42c5f2024e6119741fa29aa207d74ab62
Peter Kozhevnikov <kozhevnikov@myriad-corp.com>
2017-07-16 21:28:27 +0300

    Add docs

--- source/dich/moduleinterface.d
+++ source/dich/moduleinterface.d
@@ -1,7 +1,12 @@
-
+/**
+	Copyright: 2017 © LLC CERERIS
+	License: Subject to the terms of the MIT license, as written in the included LICENSE.txt file.
+	Authors: LLC CERERIS
+*/
 
 module dich.moduleinterface;
 
+/// Interface for implementing modules
 interface ModuleInterface
 {
 }
